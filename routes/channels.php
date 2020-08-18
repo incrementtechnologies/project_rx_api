e882507160602636b611d952ec8efd80e3012cf8
kennCK <kennettecanales@gmail.com>
2020-08-18 23:37:37 +0800

    update

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,9 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+Broadcast::channel('runway', function ($user) {
+    return true;
+});
+Broadcast::channel('private-runway', function ($user) {
+    return true;
+});
