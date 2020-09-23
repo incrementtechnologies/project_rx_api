6861762f6927cf7c662bd8704b3126a6af4c1106
Ean Jason <ecvelayo@gmail.com>
2020-09-24 05:47:32 +0800

    PayloadController

---
+++ app/Http/Controllers/PayloadController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\APIController;
+use Increment\Imarket\Coupon\Models\Coupon;
+use Increment\Imarket\Reward\Models\Reward;
